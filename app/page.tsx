fa914db539c27570df0c8878a8893ce9f2c7d4c5
antoshia2n <gameister1@gmail.com>
2026-03-04 22:36:09 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,19 @@ import { supabaseServer } from '@/lib/supabaseServer'
 import SalesManager from '@/components/SalesManager'
 
 export default async function Page() {
-  const [salesRes, expensesRes, strategyRes] = await Promise.all([
-    supabaseServer.from('sm_sales').select('*').order('created_at', { ascending: true }),
+  const [contractsRes, paymentsRes, singlesRes, expensesRes, strategyRes] = await Promise.all([
+    supabaseServer.from('sm_contracts').select('*').order('created_at', { ascending: true }),
+    supabaseServer.from('sm_payments').select('*').order('month_idx', { ascending: true }),
+    supabaseServer.from('sm_singles').select('*').order('month_idx', { ascending: true }),
     supabaseServer.from('sm_expenses').select('*').order('created_at', { ascending: true }),
     supabaseServer.from('sm_strategy').select('*'),
   ])
 
   return (
     <SalesManager
-      initialSales={salesRes.data ?? []}
+      initialContracts={contractsRes.data ?? []}
+      initialPayments={paymentsRes.data ?? []}
+      initialSingles={singlesRes.data ?? []}
       initialExpenses={expensesRes.data ?? []}
       initialStrategy={strategyRes.data ?? []}
     />
